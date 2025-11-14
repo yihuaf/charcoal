2772a87cf3e6f8084c3b954de180f2f2d16b7d03
yihuaf <yihuaf@cloudflare.com>
2025-11-14 15:50:51 -0800

    Add create, checkout, delete, rename to top level command

---
+++ apps/cli/src/commands/delete.ts
@@ -0,0 +1,33 @@
+import yargs from 'yargs';
+import { deleteBranchAction } from '../actions/delete_branch';
+import { graphite } from '../lib/runner';
+
+const args = {
+  name: {
+    type: 'string',
+    positional: true,
+    demandOption: true,
+    optional: false,
+    describe: 'The name of the branch to delete.',
+    hidden: true,
+  },
+  force: {
+    describe: `Delete the branch even if it is not merged or closed.`,
+    demandOption: false,
+    type: 'boolean',
+    alias: 'f',
+    default: false,
+  },
+} as const;
+type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
+
+export const aliases = ['dl'];
+export const command = 'delete [name]';
+export const canonical = 'branch delete';
+export const description =
+  'Delete a branch and its corresponding Charcoal metadata.';
+export const builder = args;
+export const handler = async (argv: argsT): Promise<void> =>
+  graphite(argv, canonical, async (context) =>
+    deleteBranchAction({ branchName: argv.name, force: argv.force }, context)
+  );
---
+++ apps/cli/src/commands/rename.ts
@@ -0,0 +1,44 @@
+import yargs from 'yargs';
+import { renameCurrentBranch } from '../actions/rename_branch';
+import { ExitFailedError } from '../lib/errors';
+import { graphite } from '../lib/runner';
+
+const args = {
+  name: {
+    describe: `The new name for the current branch.`,
+    demandOption: false,
+    type: 'string',
+    positional: true,
+    hidden: true,
+  },
+  force: {
+    describe: `Allow renaming a branch that is already associated with an open GitHub pull request.`,
+    demandOption: false,
+    type: 'boolean',
+    alias: 'f',
+    default: false,
+  },
+} as const;
+type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
+
+export const command = 'rename [name]';
+export const aliases = ['rn'];
+export const canonical = 'branch rename';
+export const description =
+  'Rename a branch and update metadata referencing it. If no branch name is supplied, you will be prompted for a new branch name. Note that this removes any associated GitHub pull request.';
+export const builder = args;
+
+export const handler = async (args: argsT): Promise<void> => {
+  return graphite(args, canonical, async (context) => {
+    if (!args.name && !context.interactive) {
+      throw new ExitFailedError(
+        `Please supply a new branch name when in non-interactive mode`
+      );
+    }
+
+    await renameCurrentBranch(
+      { newBranchName: args.name, force: args.force },
+      context
+    );
+  });
+};
