f7496050dfca9796ef3804d879a9a535bb3f9131
yihuaf <yihuaf@cloudflare.com>
2025-11-18 14:53:50 -0800

    upgrade to node 22

--- apps/cli/scripts/node_version.js
+++ apps/cli/scripts/node_version.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const requiredVersion = '>=v14';
+const requiredVersion = '>=v22';
 const semver = require('semver');
 try {
  if (!semver.satisfies(process.version, requiredVersion)) {
@@ -11,5 +11,5 @@ try {
     process.exit(1);
   }
 } catch {
-  console.error(`Unable to validate Node.js version.  Note that Charcoal requires v14 or higher.`)
+  console.error(`Unable to validate Node.js version.  Note that Charcoal requires v22 or higher.`)
 }
